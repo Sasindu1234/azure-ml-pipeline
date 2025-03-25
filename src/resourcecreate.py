a7e466fd5ceb55584d336ba5cd71ac22ef3bd672
Sasindu1234 <saindu.rashmina@gmail.com>
2025-03-25 13:25:19 +0530

    path changesss

--- src/resourcecreate.py
+++ src/resourcecreate.py
@@ -120,7 +120,7 @@ def create_environment(subscription_id, resource_group,workspace_name):
 
 
     # Define the environment
-    env_name = "clustertestingenvironment"
+    env_name = "clustertestingenvironments"
     env = Environment(
         image="mcr.microsoft.com/azureml/openmpi4.1.0-ubuntu20.04",
         name=env_name,
@@ -160,6 +160,8 @@ if __name__ == "__main__":
         # Call functions with configuration values
     print(f"Creating ML resources for workspace '{workspace_name}'...")
     # Call functions with configuration values
+    
+    """
     create_ml_resources(
         subscription_id=config["subscription_id"],
         resource_group=config["resource_group"],
@@ -167,9 +169,8 @@ if __name__ == "__main__":
         compute_instance_name = config["compute_instance_name"],
         compute_cluster_name = config["compute_cluster_name"],
         location=config["location"],
-        
-        
     )
+"""
 
     create_containers_and_upload_files(
         connect_str=config["connect_str"],
