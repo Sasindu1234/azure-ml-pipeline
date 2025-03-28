2e801f44f30781bcde7aaeb687f83c4838cc8923
Sasindu1234 <saindu.rashmina@gmail.com>
2025-03-28 14:55:03 +0530

    try new deploy14

--- src/resourcecreate.py
+++ src/resourcecreate.py
@@ -191,7 +191,7 @@ if __name__ == "__main__":
             "query2.csv": "data/query2.csv"
         },
         "countainer_namerun": "rawdata",
-        "compute_instance_name": "sasindu7",
+        "compute_instance_name": "sasindu8",
         "compute_cluster_name" : "testone"
     }
 
